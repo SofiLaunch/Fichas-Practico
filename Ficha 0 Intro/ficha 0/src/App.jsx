556290b1eb20dd853ab5f2da303b332abb835fb2
SofiLaunch <soffiem.launch@gmail.com>
2026-04-06 20:06:03 -0300

    Ficha 0 Terminada

--- Ficha 0 Intro/ficha 0/src/App.jsx
+++ Ficha 0 Intro/ficha 0/src/App.jsx
@@ -1,133 +1,45 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import "./App.css";
 import Tarjeta from "./components/Tarjeta";
-import Wade from "./components/Wade";
-import Start from "./components/Start";
-import Zeze from "./components/Zeze";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Tarjeta name="Sabrina" descripcion="General" link="Ver"></Tarjeta>
-      <Wade></Wade>
-      <Start></Start>
-      <Zeze></Zeze>
+      <div className="contenedor-tarjetas">
+        <Tarjeta
+          name="Antroposofía"
+          subtitulo=""
+          descripcion="La antroposofía, desarrollada por Rudolf Steiner a principios del siglo
+        XX, es una ciencia espiritual  que busca conectar la sabiduría humana
+        con el ámbito espiritual universal, promoviendo una visión holística del
+        ser humano cuerpo, alma y espíritu. Sus aplicaciones prácticas
+        incluyen la educación Waldorf, agricultura biodinámica y medicina
+        antroposófica."
+          url="https://wakeupplatform.com/wp-content/uploads/2024/07/metafisica-espiritual.webp"
+          link="https://antroposofiamexico.org/la-antroposofia/"
+        ></Tarjeta>
+        <Tarjeta
+          name="Desarollo de Software"
+          subtitulo="Descripción del software"
+          descripcion="El desarrollo de software hace referencia a un conjunto de actividades informáticas dedicadas al proceso de creación, diseño, implementación y soporte de software.
+          El software puede diseñarse como software personalizado o comercial. El desarrollo de software a medida consiste en diseñar, crear, implementar y mantener software para un conjunto específico de usuarios, funciones u organizaciones.
+          Los programadores, ingenieros de software y desarrolladores de software se dedican principalmente al desarrollo de software. Estos roles interactúan, se superponen y tienen requisitos similares, como escribir código y probar software. La dinámica entre ellos varía mucho según los departamentos de desarrollo y las organizaciones."
+          url="https://colombia.unir.net/wp-content/uploads/sites/4/2025/01/Tendencias-en-desarrollo-de-software2.jpg"
+          link="https://www.ibm.com/es-es/think/topics/software-development"
+        ></Tarjeta>
+        <Tarjeta
+          name="Metafísica"
+          subtitulo="¿Qué es la Metafísica Espiritual?"
+          descripcion="La metafísica espiritual es una rama de la filosofía que se adentra en lo que va más allá de la realidad física. Se ocupa de preguntas fundamentales sobre el ser, la existencia y el significado de la vida. A través de prácticas como la meditación, la lectura de registros akáshicos y el asesoramiento espiritual, las personas se sumergen en una exploración profunda de su alma y su conexión con el universo.
+          
+          Desde la perspectiva de la metafísica espiritualista, el ser humano y su energía no se limita a su cuerpo físico, sino que posee una esencia espiritual que trasciende lo material. Esta visión propone que la realidad es multidimensional, compuesta por planos de existencia que van más allá de lo perceptible por los sentidos físicos.
 
-      <br />
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Sofia - Desarrollo de Software</h1>
-          <p>
-            El desarrollo de software es el proceso multidisciplinario de
-            concebir, diseñar, programar, probar y mantener aplicaciones,
-            frameworks u otros componentes informáticos. Involucra un ciclo de
-            vida (SDLC) que abarca desde la planificación y análisis de
-            requisitos hasta la implementación y soporte continuo, asegurando
-            soluciones funcionales, escalables y eficientes.
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+La naturaleza del ser, según esta filosofía, es fundamentalmente espiritual. El cuerpo físico se considera un vehículo temporal para el espíritu, que es eterno y evoluciona a través de experiencias en diferentes planos de existencia. Esta concepción implica que la realidad última es de carácter espiritual, y que el mundo material es una manifestación o expresión de esta realidad superior."
+          url="https://wakeupplatform.com/wp-content/uploads/2024/07/espiritualidad-en-metafisica.webp"
+          link="https://reikiesmas.com/servicios/metafisica-espiritual/"
+        ></Tarjeta>
+      </div>
     </>
   );
 }
